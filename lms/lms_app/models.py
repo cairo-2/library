e9d9ad2fa0a23a2a5c133d2193b8e8edc546a8dd
cairo <abdoabdoallrk2@gmail.com>
2024-10-29 01:12:10 +0300

    the end

--- lms/lms_app/models.py
+++ lms/lms_app/models.py
@@ -18,16 +18,16 @@ class Book(models.Model):
 
 
     title = models.CharField(max_length=250)
-    author = models.CharField(max_length=250, null=True, blank=True)
-    photo_book = models.ImageField(upload_to='photo', null=True, blank=True)
-    photo_author = models.ImageField(upload_to='photo', null=True, blank=True)
+    author = models.CharField(max_length=250)
+    photo_book = models.ImageField(upload_to='photo')
+    photo_author = models.ImageField(upload_to='photo')
     pages = models.IntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     retal_price_day = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     retal_period = models.IntegerField(null=True, blank=True)
     total_rental = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     active = models.BooleanField(default=True)
-    status = models.CharField(max_length=50, choices=status_book,null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
+    status = models.CharField(max_length=50, choices=status_book)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     def __str__(self):
         return self.title
